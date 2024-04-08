ec8cf3787dbe17d2be7817732543167b55ecba93
Dan Nascimento <dannascimento93@gmail.com>
2024-04-07 21:19:22 -0400

    Add more buttons and fix bugs

--- rh-resources.js
+++ rh-resources.js
@@ -6,16 +6,15 @@ let checkPageForURL = setInterval(() => {
     getURL = window.location.href;
     
     // Check if on a stock or crypto page
-    if (getURL.includes('/stocks/') || getURL.includes('/crypto/')) {
-        // Get the ticker symbol
+    if (getURL.includes('/stocks/') || getURL.includes('/crypto/')) {        
         ticker = getTickerSymbol();
-
+        
         // If buttons haven't been created yet
         if (!buttonsCreated) {
             createButtons(ticker);
         
-        // Buttons have been created but ticker needs to be updated
-        } else if (ticker !== stocktwitsButton.textContent.split(" ")[0]) {
+        // Buttons have been created but the link is wrong
+        } else if (ticker !== seekingalphaLink.href.split('-')[0].split('/').filter(Boolean).pop().toUpperCase()) {
             removeButtons();
             createButtons(ticker);
         }
@@ -59,30 +58,58 @@ const stocktwitsButton = document.createElement("button");
 const stocktwitsLink = document.createElement("a");
 const marketwatchButton = document.createElement("button");
 const marketwatchLink = document.createElement("a");
+const finvizButton = document.createElement("button");
+const finvizLink = document.createElement("a");
+const seekingalphaButton = document.createElement("button");
+const seekingalphaLink = document.createElement("a");
+const openinsiderButton = document.createElement("button");
+const openinsiderLink = document.createElement("a");
 stocktwitsButton.classList.add("rh-resources-button-style");
 marketwatchButton.classList.add("rh-resources-button-style");
+finvizButton.classList.add("rh-resources-button-style");
+seekingalphaButton.classList.add("rh-resources-button-style");
+openinsiderButton.classList.add("rh-resources-button-style");
 
 
 function createButtons(ticker) {
-    stocktwitsButton.textContent = ticker + " StockTwits";
-    marketwatchButton.textContent = ticker + " MarketWatch";
+    stocktwitsButton.textContent = "StockTwits";
+    marketwatchButton.textContent = "MarketWatch";
+    finvizButton.textContent = "Finviz";
+    seekingalphaButton.textContent = "Seeking Alpha";
+    openinsiderButton.textContent = "OpenInsider";
     
     if (getURL.includes('/stocks/')) {
         stocktwitsLink.href = "https://stocktwits.com/symbol/" + ticker;
         marketwatchLink.href = "https://www.marketwatch.com/investing/stock/" + ticker;
+        finvizLink.href = "https://finviz.com/quote.ashx?t=" + ticker;
+        seekingalphaLink.href = "https://seekingalpha.com/symbol/" + ticker;
+        openinsiderLink.href = "http://openinsider.com/" + ticker;
     } else if (getURL.includes('/crypto/')) {
         stocktwitsLink.href = "https://stocktwits.com/symbol/" + ticker + ".X";
-        marketwatchLink.href = "https://www.marketwatch.com/investing/cryptocurrency/" + ticker + "usd";
+        marketwatchLink.href = "https://www.marketwatch.com/investing/cryptocurrency/" + ticker + "USD";
+        finvizLink.href = "https://finviz.com/crypto_charts.ashx?t=" + ticker + "USD";
+        seekingalphaLink.href = "https://seekingalpha.com/symbol/" + ticker + "-USD";
     }
 
     stocktwitsLink.target = "_blank";
     marketwatchLink.target = "_blank";
+    finvizLink.target = "_blank";
+    seekingalphaLink.target = "_blank";
+    openinsiderLink.target = "_blank";
 
     stocktwitsLink.appendChild(stocktwitsButton);
     marketwatchLink.appendChild(marketwatchButton);
+    finvizLink.appendChild(finvizButton);
+    seekingalphaLink.appendChild(seekingalphaButton);
+    openinsiderLink.appendChild(openinsiderButton);
 
     document.querySelector("h1").insertAdjacentElement("afterend", stocktwitsLink);
     document.querySelector("h1").insertAdjacentElement("afterend", marketwatchLink);
+    document.querySelector("h1").insertAdjacentElement("afterend", finvizLink);
+    document.querySelector("h1").insertAdjacentElement("afterend", seekingalphaLink);
+    if (getURL.includes('/stocks/')) {
+        document.querySelector("h1").insertAdjacentElement("afterend", openinsiderLink);
+    }
     buttonsCreated = true;
 }
 
@@ -90,5 +117,8 @@ function createButtons(ticker) {
 function removeButtons() {
     stocktwitsButton.remove();
     marketwatchButton.remove();
+    finvizButton.remove();
+    seekingalphaButton.remove();
+    openinsiderButton.remove();
     buttonsCreated = false;
 }
